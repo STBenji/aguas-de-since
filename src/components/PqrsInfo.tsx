d81e6d2df2fffadcb54f106f28f3d4ccd6812f95
ConanGH-S <blandon0207s@gmail.com>
2023-12-21 10:04:48 -0500

    fix: component pqrsinfo.tsx improved responsiveness

--- src/components/PqrsInfo.tsx
+++ src/components/PqrsInfo.tsx
@@ -2,18 +2,20 @@ import { Image } from '@nextui-org/react'
 
 export const PQRSInfo = () => {
   return (
-    <article className='grid items-center auto-rows-min'>
-      <div className='grid grid-cols-[auto_1fr] '>
-        <Image
-          width={300}
-          height={300}
-          src='/LogoPrincipal_2.png'
-          alt='Logo de la empresa'
-        />
+    <article className='grid items-center auto-rows-min gap-y-6 md:gap-y-0'>
+      <div className='grid grid-rows-2 md:grid-rows-1 md:grid-cols-[auto_1fr]'>
+        <span className='justify-self-center md:justify-self-end'>
+          <Image
+            width={300}
+            height={300}
+            src='/LogoPrincipal_2.png'
+            alt='Logo de la empresa'
+          />
+        </span>
         <h3 className='grid self-center text-3xl font-extrabold uppercase auto-rows-min'>
-          <span className='justify-self-start text-[#4ab8c7] text-shadow-[0_1.2px_1.2px_#4ab8c7]'>Peticiones,</span>
-          <span className='text-transparent justify-self-center font-outline text-shadow-[0_1.2px_1.2px_#4ab8c7]'>Quejas, Reclamos</span>
-          <span className='justify-self-center text-[#4ab8c7]'>Sugerencias y Denuncias</span>
+          <span className='justify-self-center text-center md:text-left md:pr-96 text-[#4ab8c7] text-shadow-[0_1.2px_1.2px_#4ab8c7]'>Peticiones,</span>
+          <span className='text-transparent justify-self-center text-center md:text-left font-outline text-shadow-[0_1.2px_1.2px_#4ab8c7]'>Quejas, Reclamos</span>
+          <span className='justify-self-center text-[#4ab8c7] text-center md:text-left'>Sugerencias y Denuncias</span>
         </h3>
       </div>
       <div className='max-w-screen-md mx-auto text-lg text-center min-w-min'>
